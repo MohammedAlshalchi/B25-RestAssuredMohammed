5600b461c62473492df3d5b05b4707ecc65fb1cb
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-04-26 22:34:39 -0400

    practice2

--- src/test/java/com/cydeo/HW/day3HW/Q2.java
+++ src/test/java/com/cydeo/HW/day3HW/Q2.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Q2 extends HrTestBase {
     /*
@@ -26,9 +28,12 @@ public class Q2 extends HrTestBase {
 
     @Test
     public void test2() {
-        Response response = given().accept(ContentType.JSON)
-                .and().queryParam("q","{\"department_id\":80}")
-                .when().get("/employees");
+        Response response = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParam("q","{\"department_id\":80}")
+                .when()
+                .get("/employees");
 
         assertEquals(200, response.statusCode());
         assertEquals("application/json", response.header("Content-Type"));
@@ -55,6 +60,75 @@ public class Q2 extends HrTestBase {
         assertEquals(25,count);
 
     }
+
+
+    @Test
+    public void testTESTpractice(){
+
+       Response response = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParam("q","{\"department_id\":80}")
+                .when()
+                .get("/employees");
+
+
+        System.out.println(response.statusCode());
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(response.contentType());
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(response.header("Content-Type"));
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+       // System.out.println(response.prettyPrint());
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+
+
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.contentType());
+        assertEquals("application/json",response.header("Content-Type"));
+
+        JsonPath jsonPath = response.jsonPath();
+
+
+        List<String> job_id = jsonPath.getList("items.findAll {it.job_id.startsWith(\"SA\")}.job_id");
+        System.out.println("job_id = " + job_id);
+
+        for (String s : job_id){
+            assertEquals(true,s.startsWith("SA"));
+        }
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+
+        List<Integer> department_id = jsonPath.getList("items.findAll {it.department_id == 80}.department_id") ;
+
+        System.out.println("department_id = " + department_id);
+
+        for (Integer each : department_id){
+            assertEquals(80,each);
+        }
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+
+
+int count = response.path("count");
+        System.out.println("count = " + count);
+assertEquals(25,count);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
